d35900ced214e0362ad7a41b60d42a8849d29fc1
Kacper <kacper.chanek@o2.pl>
2018-07-08 22:56:19 +0200

    New branch

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Barryvdh\Cors\HandleCors::class,
     //    \App\Http\Middleware\Admin::class,
       //  \App\Http\Middleware\Supervisor::class,
         //\App\Http\Middleware\Employee::class
