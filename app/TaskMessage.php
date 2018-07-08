519d765afcc1a667848694a1f7a2cb76ed8580be
Kacper <kacper.chanek@o2.pl>
2018-07-08 23:44:31 +0200

    ...

--- app/TaskMessage.php
+++ app/TaskMessage.php
@@ -6,33 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaskMessage extends Model
 {
-    protected $fillable=[
+    protected $fillable = [
         'task_id',
         'order_id',
         'employee_id',
         'name',
         'message',
-        'active'
+        'active',
 
     ];
 
     public function task()
     {
-        return $this->belongsTo('App\Task','task_id','id');
+        return $this->belongsTo('App\Task', 'task_id', 'id');
     }
 
     public function order()
     {
-        return $this->belongsTo('App\Order','order_id','id');
+        return $this->belongsTo('App\Order', 'order_id', 'id');
     }
 
     public function employee()
     {
-        return $this->belongsTo('App\User','employee_id','id');
+        return $this->belongsTo('App\User', 'employee_id', 'id');
     }
 
     public function supervisor()
     {
-       return $this->hasOne('App\User','id','supervisor_id');
+        return $this->hasOne('App\User', 'id', 'supervisor_id');
     }
 }
